f3cafa78cf49ecc7582635757b4a8f36c82af4cf
Matt <mattdepauw@gmail.com>
2022-09-26 15:16:38 -0700

    chugging along

--- expressjs-backend/backend.js
+++ expressjs-backend/backend.js
@@ -1,20 +1,32 @@
-const express = require('express');
+const { application } = require("express");
+const express = require("express");
 const app = express();
 const port = 5000;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
 
-app.get('/users', (req, res) => {
+app.get("/users", (req, res) => {
+  const name = req.query.name;
+  if (name != undefined) {
+    let result = findUserByName(name);
+    result = { users_list: result };
+    res.send(result);
+  } else {
     res.send(users);
+  }
 });
+
+const findUserByName = (name) => {
+  return users["users_list"].filter((user) => user["name"] === name);
+};
 
 const users = {
   users_list: [
@@ -45,4 +57,24 @@ const users = {
     },
   ],
 };
+
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined || result.length == 0)
+    res.status(404).send("Resource not found.");
+  else {
+    result = { users_list: result };
+    res.send(result);
+  }
+});
+
+function findUserById(id) {
+  return users["users_list"].find((user) => user["id"] === id); // or line below
+  //return users['users_list'].filter( (user) => user['id'] === id);
+}
+
+
+
 
