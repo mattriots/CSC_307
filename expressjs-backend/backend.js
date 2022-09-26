fc83d0ed34979120f7dd3dcbb3907c9ee4703f51
Matt <mattdepauw@gmail.com>
2022-09-26 14:46:28 -0700

    adding users

--- expressjs-backend/backend.js
+++ expressjs-backend/backend.js
@@ -11,3 +11,38 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
+
+app.get('/users', (req, res) => {
+    res.send(users);
+});
+
+const users = {
+  users_list: [
+    {
+      id: "xyz789",
+      name: "Charlie",
+      job: "Janitor",
+    },
+    {
+      id: "abc123",
+      name: "Mac",
+      job: "Bouncer",
+    },
+    {
+      id: "ppp222",
+      name: "Mac",
+      job: "Professor",
+    },
+    {
+      id: "yat999",
+      name: "Dee",
+      job: "Aspring actress",
+    },
+    {
+      id: "zap555",
+      name: "Dennis",
+      job: "Bartender",
+    },
+  ],
+};
+
